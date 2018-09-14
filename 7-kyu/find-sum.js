60173d60a537d11b2417aa2ad3c7d5d34853b526
artbohr <bohrcs@gmail.com>
2018-09-14 04:17:13 +0200

    Create find-sum.js

---
+++ 7-kyu/find-sum.js
@@ -0,0 +1,34 @@
+function diagonalSum(matrix){
+
+let total = 0;
+
+  for (x=0;x<matrix.length;x++){
+    total += matrix[x][x];
+  }
+
+  return total;
+}
+
+/*
+Given a "square" array of subarrays, find the sum of values from the first value of the first array, the second value of the second array, the third value of the third array, and so on...
+
+Example 1:
+
+var exampleArray = [[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]]
+
+diagonalSum(exampleArray) // => 4
+
+Example 2:
+
+var exampleArray = [[1, 0, 0, 0, 0],
+                   [0, 1, 0, 0, 0],
+                   [0, 0, 1, 0, 0],
+                   [0, 0, 0, 1, 0],
+                   [0, 0, 0, 0, 1]]
+
+diagonalSum(exampleArray) // => 5
+
+ */
