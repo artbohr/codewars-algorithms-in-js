006743e992c124694b7aa558e995ba8303d7ca9e
artbohr <bohrcs@gmail.com>
2019-08-30 02:36:48 +0200

    Create return-missing.js

---
+++ 7-kyu/return-missing.js
@@ -0,0 +1,17 @@
+function getMissingElement(superImportantArray){
+    return 45-superImportantArray.reduce((a,b)=>a+b);
+  }
+
+/*
+Fellow code warrior, we need your help! We seem to have lost one of our array elements,
+ and we need your help to retrieve it! Our array, superImportantArray, was supposed to
+contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+Write a function called getMissingElement that accepts an array of unique integers
+ between 0 and 9 (inclusive), and returns the missing element.
+ 
+Examples:
+
+getMissingElement( [0, 5, 1, 3, 2, 9, 7, 6, 4] ) // returns 8
+getMissingElement( [9, 2, 4, 5, 7, 0, 8, 6, 1] ) // returns 3
+*/
