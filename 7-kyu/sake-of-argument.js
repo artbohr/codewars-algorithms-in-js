1f60838ab85b0381daebb8591dd9abc8425688d8
artbohr <bohrcs@gmail.com>
2019-09-03 06:40:14 +0200

    Create sake-of-arguments.js

---
+++ 7-kyu/sake-of-argument.js
@@ -0,0 +1,15 @@
+function numbers(){
+  return [...arguments].filter(x => typeof x == 'number').length == arguments.length;
+}
+
+/*
+Write a function named numbers that returns true if all the parameters it is
+ passed are of the Number type. Otherwise, the function should return false.
+ The function should accept any number of parameters.
+
+Example usage:
+
+numbers(1, 4, 3, 2, 5); // true
+numbers(1, "a", 3); // false
+numbers(1, 3, NaN); // true
+*/
