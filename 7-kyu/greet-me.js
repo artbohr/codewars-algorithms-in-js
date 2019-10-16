3cd12af170474fc97a99e1b967fff47a25e309c9
artbohr <bohrcs@gmail.com>
2019-10-16 16:04:24 +0200

    greet-me.js

---
+++ 7-kyu/greet-me.js
@@ -0,0 +1,13 @@
+var greet = function(name) {
+    return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
+  };
+
+/*
+Write a method that takes one argument as name and then greets that name, capitalized
+ and ends with an exclamation point.
+
+Example:
+
+greet("maya") # returns "Hello Maya!"
+greet("JACK") # returns "Hello Jack!"
+*/
