358dc9f828b568d6ce04947640b94b445a3546ae
artbohr <bohrcs@gmail.com>
2019-10-17 15:41:53 +0200

    Create array-capping.js

---
+++ 7-kyu/array-capping.js
@@ -0,0 +1,14 @@
+function capMe(names) {
+    return names.map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())
+  }
+
+
+/*
+Create a method that accepts an array of names, and returns an array of each
+ name with its first letter capitalized.
+
+example
+
+capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+*/
