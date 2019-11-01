d3bdedbd8d9d6d2a876a1137b29ebb9df7e12079
artbohr <bohrcs@gmail.com>
2019-11-01 01:09:20 +0100

    Create working-with-arrays.js

---
+++ 7-kyu/working-with-arrays.js
@@ -0,0 +1,9 @@
+function withoutLast(arr) {
+    return arr.slice(0,-1);
+}
+
+/*
+In this kata the function returns an array/list of numbers without its last element.
+ The function is already written for you and the basic tests pass,
+ but random tests fail. Your task is to figure out why and fix it.
+*/
